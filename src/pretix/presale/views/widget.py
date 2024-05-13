6d6a069430bc8170fd635c7c819258ce454f73c2
Raphael Michel <michel@rami.io>
2024-05-13 16:26:42 +0200

    Fix more tests

--- src/pretix/presale/views/widget.py
+++ src/pretix/presale/views/widget.py
@@ -253,7 +253,7 @@ class WidgetAPIProductList(EventListMixin, View):
             self.request.event,
             subevent=self.subevent,
             voucher=self.voucher,
-            channel=self.request.sales_channel.identifier,
+            channel=self.request.sales_channel,
             base_qs=qs,
             require_seat=None,
             memberships=(
